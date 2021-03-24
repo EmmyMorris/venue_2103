471128b279b9830e6691cd833a1b887d89cbf137
Emmy Morris <emmymorris1102@gmail.com>
2021-03-24 16:02:18 -0600

    Iteration 1 and 2

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,2 +1,21 @@
 class Venue
+  attr_reader :name, :capacity, :patrons
+  def initialize(name, capacity)
+    @name     = name
+    @capacity = capacity
+    @patrons  = []
+  end
+
+  def add_patron(patron)
+    @patrons << patron
+  end
+
+  def yell_at_patrons
+    @patrons << add_patron.upcase
+  end
+
+  #def over_capacity
+    #@patrons >= @capacity
+  #end
+
 end
